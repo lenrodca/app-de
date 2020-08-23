4d2f5482cc0bc120758f7b9602652f694d302855
russimac <69682485+russimac@users.noreply.github.com>
2020-08-22 22:21:57 -0500

    Prueba  2

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-//Prueba de commit
+//Prueba de commit	
+//Prueba 2 de commit
+
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
